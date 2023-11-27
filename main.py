1446eeb29ee28fd7fadbb96dff5f509e28d40e81
Adithya Awati <1ds21ai001@dsce.edu.in>
2023-11-27 11:38:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods="GET", "POST",
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
@@ -11,3 +20,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/env")
+async def print_variables():
+    return {"TEST": os.environ["TEST"]}
