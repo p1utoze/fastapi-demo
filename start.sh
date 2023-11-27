0e3c87ec8a4db67575c0599220d2bbd9988e3640
Adithya Awati <1ds21ai001@dsce.edu.in>
2023-11-27 09:04:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker build -t farmsense -f Dockerfile .
-docker run -d -p 80:8080 --env-file .env.prod  farmsense
+docker build -t python-app -f Dockerfile .
+docker run -d -p 80:8080 --env-file .env.prod python-app
